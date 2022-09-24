9d505538986b07aa9af459078b990418a17da50c
klimma1508 <martin.klims1@gmail.com>
2022-09-24 22:26:48 +0200

    Prepare to send data from actual database to MongoDB

--- main.py
+++ main.py
@@ -15,10 +15,6 @@ def main():
             lcsc = dat[6]
 
 
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
